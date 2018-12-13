c9d533414ce02564b4a9fb25a34fa72ce5d56342
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-13 14:47:23 +0200

    <fix> image host

--- react-client/src/components/Home/HomeClass.js
+++ react-client/src/components/Home/HomeClass.js
@@ -11,7 +11,25 @@ class HomeClass extends React.Component {
         super(props)
         this.myRef = React.createRef();
         this.state = {
-            items: [],
+            items: [{
+                "_id": {
+                    "$oid": "5c124b0afc9e7108992d49fc"
+                },
+                "eventLocation": [
+                    ""
+                ],
+                "attending": [],
+                "creatorName": "jkdnas",
+                "eventName": "lkqwdq",
+                "cost": 2,
+                "des": "sdcscawc",
+                "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDA4Jhlt2TGWKs8hSYa4yLTv26x7UqLoVtCbcbh1KNxPjbuo8Ibw",
+                "availableSeats": 3,
+                "date": "2018-12-21T00:12",
+                "email": "isa@isa.com",
+                "imgName": "http://localhost:4000/images/myImage-1544702722136.jpg",
+                "__v": 0
+            }],
           }
     }
 
