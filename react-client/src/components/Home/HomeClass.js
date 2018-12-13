a36fb5f7e590301c5e1cb5a80fe881a94467fa55
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-13 15:02:43 +0200

    <fix> image host

--- react-client/src/components/Home/HomeClass.js
+++ react-client/src/components/Home/HomeClass.js
@@ -11,25 +11,7 @@ class HomeClass extends React.Component {
         super(props)
         this.myRef = React.createRef();
         this.state = {
-            items: [{
-                "_id": {
-                    "$oid": "5c124b0afc9e7108992d49fc"
-                },
-                "eventLocation": [
-                    ""
-                ],
-                "attending": [],
-                "creatorName": "jkdnas",
-                "eventName": "lkqwdq",
-                "cost": 2,
-                "des": "sdcscawc",
-                "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDA4Jhlt2TGWKs8hSYa4yLTv26x7UqLoVtCbcbh1KNxPjbuo8Ibw",
-                "availableSeats": 3,
-                "date": "2018-12-21T00:12",
-                "email": "isa@isa.com",
-                "imgName": "http://localhost:4000/images/myImage-1544702722136.jpg",
-                "__v": 0
-            }],
+            items: [],
           }
     }
 
@@ -63,7 +45,12 @@ class HomeClass extends React.Component {
                     <Vision />
                     <div classsname="images">
                         <p className="main_title"><span>Events</span></p>
-                        <EventClassNew item={this.state.items} />
+                        {    this.state.items.map((item) => {
+                                return (<div ref={this.myRef}>
+                                    <EventClassNew item={item} />
+                                </div>)
+                            })
+                        }
                     </div>
                 </div>
             </div>
