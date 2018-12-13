abdd100376a46c98c6542a3bc3d93e9158090557
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-13 14:18:05 +0200

    <fix> image host

--- server.js
+++ server.js
@@ -44,10 +44,10 @@ db.once('open', function () {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'react-client/public')));
+  app.use(express.static(path.join(__dirname, 'react-client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'react-client/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'));
   });
 }
 
