4f09c35157224d7844b378c044e4011797fb6344
IssaAnagreh <isa_anagreh@hotmail.com>
2018-12-13 15:09:10 +0200

    <fix> image host

--- react-client/src/components/Home/HomeClass.js
+++ react-client/src/components/Home/HomeClass.js
@@ -11,8 +11,26 @@ class HomeClass extends React.Component {
         super(props)
         this.myRef = React.createRef();
         this.state = {
-            items: [],
-          }
+            items: [{
+                "_id": {
+                    "$oid": "5c1257bb7597870016116379"
+                },
+                "eventLocation": [
+                    ""
+                ],
+                "attending": [],
+                "creatorName": "Issa",
+                "eventName": "Issa",
+                "cost": 23,
+                "des": "Issa",
+                "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDA4Jhlt2TGWKs8hSYa4yLTv26x7UqLoVtCbcbh1KNxPjbuo8Ibw",
+                "availableSeats": 232,
+                "date": "2018-12-20T00:12",
+                "email": "isa@isa.com",
+                "imgName": "http://zahgaan.herokuapp.com/images/myImage-1544705958891.png",
+                "__v": 0
+            }],
+        }
     }
 
     scrollToMyRef = () => {
@@ -45,12 +63,9 @@ class HomeClass extends React.Component {
                     <Vision />
                     <div classsname="images">
                         <p className="main_title"><span>Events</span></p>
-                        {    this.state.items.map((item) => {
-                                return (<div ref={this.myRef}>
-                                    <EventClassNew item={item} />
-                                </div>)
-                            })
-                        }
+                        
+                                    <EventClassNew item={this.state.items} />
+
                     </div>
                 </div>
             </div>
